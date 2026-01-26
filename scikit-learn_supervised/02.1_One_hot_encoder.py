3f635403f56f57c0346fce1c032547c109077807
bvijay5 <b.vijayabhaskarraju@gmail.com>
2026-01-26 11:45:52 +0530

    Linear Regression

---
+++ scikit-learn_supervised/02.1_One_hot_encoder.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+
+d = {
+    'sales': [100000,222000,1000000,522000,111111,222222,1111111,20000,75000,90000,1000000,10000],
+    'city': ['Tampa','Tampa','Orlando','Jacksonville','Miami','Jacksonville','Miami','Miami',
+             'Orlando','Orlando','Orlando','Orlando'],
+    'size': ['Small', 'Medium','Large','Large','Small','Medium','Large','Small','Medium','Medium',
+             'Medium','Small',]
+}
+
+df = pd.DataFrame(d)
+
